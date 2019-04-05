309e16fb15c52e3b7f8dc38cdedd499e1208e0a0
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-05 11:37:23 -0400

    add get opening crawl

--- swapi.js
+++ swapi.js
@@ -1,1 +1,20 @@
 // Write your swapi code in this file!
+
+document.querySelector('#crawlBtn').addEventListener('click', getOpeningCrawl)
+
+function getOpeningCrawl(){
+  fetch('https://swapi.co/api/films/1/')
+    .then(res => res.json())
+    .then(json => insertCrawl(json.opening_crawl));
+}
+
+function insertCrawl(input){
+  let crawlDiv = document.querySelector('#crawlDiv')
+  let newP = document.createElement('p');
+  crawlDiv.appendChild(newP)
+  newP.innerText = input;
+}
+
+function getPlanet(){
+
+}
