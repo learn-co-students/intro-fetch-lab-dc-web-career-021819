a885a3d17048df08886bd1e1a43876527c68198d
mmorales4050 <mmorales4050@gmail.com>
2019-04-08 12:12:38 -0400

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,58 @@
-// Write your numbers code in this file!
+document.addEventListener('DOMContentLoaded', init)
+
+function init(){
+
+  //listeners
+  document.addEventListener('click', clickHandler)
+  document.addEventListener('change', changeHandler)
+
+  //variables
+  const factsAboutOneButton = document.getElementById("number-one")
+  const oneFactsDiv = document.getElementById("one-facts")
+  const numberInput = document.getElementById("pick-a-number")
+  const randomFactDiv = document.getElementById("random-math-fact")
+  const yearHistoryDiv = document.getElementById("year-history")
+  const allNumbersButton = document.getElementById("all-numbers-button")
+  const allNumbersDiv = document.getElementById("all-the-numbers")
+  let year = 2019
+
+  //interval
+  setInterval(() => {
+    fetch(`http://numbersapi.com/${year}/year`)
+    .then(response => response.text())
+    .then(response => {yearHistoryDiv.innerText = response})
+    year--
+  }, 5000)
+  //functions
+  function clickHandler(event){
+    if (event.target === factsAboutOneButton){
+      displayRandomFactAboutNumber(1, oneFactsDiv)
+    }
+    if (event.target === allNumbersButton) {
+      allNumbersDiv.innerHTML = "";
+      [...Array(100).keys()].forEach((i) => {
+        fetch(`http://numbersapi.com/random/trivia`)
+        .then(response => response.text())
+        .then(response => {allNumbersDiv.innerHTML += `<div>${response}</div>`})
+    })
+    }
+  }
+
+  function changeHandler(event){
+    if (event.target === numberInput){
+      number = event.target.value
+      if (!isNaN(Number(number))){
+        displayRandomFactAboutNumber(number, randomFactDiv)
+      }
+      else {
+        randomFactDiv.innerText = "Pick a valid number"
+      }
+    }
+  }
+
+  function displayRandomFactAboutNumber(number, location){
+    fetch(`http://numbersapi.com/${number}/trivia`)
+    .then(response => response.text())
+    .then(response => {location.innerText = response})
+  }
+}
--- swapi.js
+++ swapi.js
@@ -1,45 +1,52 @@
-// Write your swapi code in this file!
+// Write your numbers code in this file!
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#crawlBtn').addEventListener('click', getOpeningCrawl)
-    document.querySelector('#planetForm').addEventListener('submit', getPlanet)
-})
+document.addEventListener('DOMContentLoaded', allTheStuff);
 
-function getOpeningCrawl(e) {
-    e.preventDefault()
-    const crawlDiv = document.querySelector('#crawlDiv')
-    fetch('https://swapi.co/api/films/1')
-        .then(res => res.json())
-        .then(json => {
-            crawlText = document.createElement('p')
-            crawlText.innerText = json.opening_crawl
-            crawlDiv.appendChild(crawlText)
-        })
+function allTheStuff(e){
+  displayDroid(2)
+  displayDroid(3)
+
+  let crawlButton = document.getElementById('crawlBtn');
+  crawlButton.addEventListener('click', getOpeningCrawl);
+
+  let form = document.getElementById("planetForm");
+  form.addEventListener('submit', getPlanet);
+
+
+}
+
+function displayDroid(number) {
+  fetch(`https://swapi.co/api/people/${number}`)
+  .then(p => p.json())
+  .then (p => {
+    droidDiv = document.getElementById(`droid-${number}`)
+    doidDiv.innerHTML =
+    `<div>${p.name}</div><div>${p.height}</div><div>${p.mass}</div><button id="droid-${number}">Get Home Planet</button>`
+  })
 }
 
 function getPlanet(e) {
-    e.preventDefault()
-    const planetId = document.querySelector('#planetInput').value
-    if (planetId > 60 || planetId < 1) {
-        alert('Invalid Input. Choose a number betwen 1 and 60.')
-        return
-    }
-    fetch(`https://swapi.co/api/planets/${planetId}`)
-        .then(res => res.json())
-        .then(json => {
-            debugger
-            const planetDiv = document.querySelector('#planetData')
+  e.preventDefault();
+  searchText = e.target.firstElementChild.value;
+  if (!(searchText <= 60) || searchText < 1 || searchText === undefined) {
+    e.target.firstElementChild.value = ""
+    alert("Enter a number between 1 and 60")
+    return
+  }
+  fetch(`https://swapi.co/api/planets/${searchText}`)
+  .then(p => p.json())
+  .then (p => {
+    document.querySelector("#planetData").innerHTML = `<p>${p.name}</p>${p.climate}`
+    e.target.firstElementChild.value = ""
 
-            const planetHeader = document.createElement('h2')
-            planetHeader.innerHTML = '<br>Planet Result'
-            planetDiv.appendChild(planetHeader)
+  })
+}
 
-            const planetName = document.createElement('h4')
-            planetName.innerText = `Name: ${json.name}`
-            planetDiv.appendChild(planetName)
-
-            const planetClimate = document.createElement("p");
-            planetClimate.innerText = `Climate: ${json.climate}`
-            planetDiv.appendChild(planetClimate)
-        })
+function getOpeningCrawl(){
+  let promiseForData = fetch('https://swapi.co/api/films/1/')
+  .then(res  => res.json())
+  .then(data => {
+    let div = document.querySelector('#crawlDiv');
+    div.innerText = data.opening_crawl;
+ });
 }
